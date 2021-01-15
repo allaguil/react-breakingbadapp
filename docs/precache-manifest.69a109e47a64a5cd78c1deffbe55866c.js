ad64f6ee1926f9b6b32f1813f6e52d48e8d00bf5
Allan Aguilar <alan-joc@hotmail.com>
2021-01-15 09:22:32 -0600

    docs added

---
+++ docs/precache-manifest.69a109e47a64a5cd78c1deffbe55866c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a2dc93454a11266b260e88f82d344d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "389221156e62935c8daa",
+    "url": "/static/css/main.239d13cc.chunk.css"
+  },
+  {
+    "revision": "757d8c2f5850f7df7f7b",
+    "url": "/static/js/2.84b4d17d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.84b4d17d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389221156e62935c8daa",
+    "url": "/static/js/main.70b2cf01.chunk.js"
+  },
+  {
+    "revision": "a964bc697d914d59f04e",
+    "url": "/static/js/runtime-main.c13a616d.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
